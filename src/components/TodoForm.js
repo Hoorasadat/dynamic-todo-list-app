ae8d89aafd54ad599d609a956bc162057e227483
Hoora <hoora@Hooras-MacBook-Pro.local>
2023-08-02 13:33:00 -0600

    add form validation

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,18 +1,41 @@
+import { useState } from 'react';
 import './TodoForm.css';
 
 function TodoForm( { onTaskAdd }) {
 
+	const [errors, setErrors] = useState([]);
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		onTaskAdd(event.target.task.value);
+		onTaskAdd(event.target.task.value.trim());
 		event.target.task.value = '';
+	}
+
+	const handleValidation = (event) => {
+		const newErrors = [];
+		const { name, value } = event.target;
+
+
+		if (name === 'task' && value.trim() === '') {
+			newErrors.push('Task cannot be empty');
+		}
+		setErrors(newErrors);
 	}
 
 	return (
 		<>
 			<form onSubmit={ handleSubmit }>
-				<input type="text" name="task" placeholder="Type a new task here"/>
-				<button>Add&nbsp;Task</button>
+				<div className="form-group">
+					<input
+						type="text"
+						name="task"
+						placeholder="Type a new task here"
+						onChange={handleValidation}
+					/>
+
+					<button type='submit' disabled={errors.length > 0}>Add&nbsp;Task</button>
+				</div>
+				{ errors.length > 0 && errors.map((error, index) => <li key={ index } className="error">{ error }</li> ) }
 			</form>
 		</>
 	);
